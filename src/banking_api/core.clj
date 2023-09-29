a48686c9e4ab5d3c5e3c9f146b088334bf4c69a6
Konrad Szydlo <konrad_szydlo@yahoo.co.uk>
2023-09-29 09:10:21 +0200

    Add basic routing using reitit

--- src/banking_api/core.clj
+++ src/banking_api/core.clj
@@ -1,5 +1,79 @@
 (ns banking-api.core
+  {:author "Konrad Szydlo"}
+  (:require [malli.util :as mu]
+            [muuntaja.core :as m]
+            [reitit.coercion.malli]
+            [reitit.dev.pretty :as pretty]
+            [reitit.ring :as ring]
+            [reitit.ring.coercion :as coercion]
+            [reitit.ring.malli]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.ring.middleware.exception :as exception]
+            [reitit.ring.middleware.parameters :as parameters]
+            [reitit.ring.spec :as spec]
+            [ring.adapter.jetty :as jetty])
   (:gen-class))
+
+(def ^:private min-account-number "should be at least 1")
+
+(def ^:private AccountId [:map [:id [:int {:min 1 :error/message min-account-number}]]])
+
+(def account-id AccountId)
+
+(def ^:private AccountResponse
+  [:map
+   [:account-number pos-int?]
+   [:name :string]
+   [:balance int?]])
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/account"
+        {:tags #{"account"}}
+        ["/:id" {:parameters {:path account-id}}
+         [""
+          {:get {:summary   "get account details"
+                 :responses {200 {:body AccountResponse}}
+                 :handler   (fn [{{{:keys [id]} :path} :parameters}]
+                              {:status 200
+                               :body {:account-number id
+                                      :balance        (+ 10 id)
+                                      :name (str "account " id)}})}}]]]]
+      {:validate spec/validate ;; enable spec validation for route data
+       :exception pretty/exception
+       :data {:coercion (reitit.coercion.malli/create
+                          {;; set of keys to include in error messages
+                           :error-keys #{#_:type :coercion :in :schema :value :errors :humanized #_:transformed}
+                           ;; schema identity function (default: close all map schemas)
+                           :compile mu/closed-schema
+                           ;; strip-extra-keys (effects only predefined transformers)
+                           :strip-extra-keys true
+                           ;; add/set default values
+                           :default-values true
+                           ;; malli options
+                           :options nil})
+              :muuntaja m/instance
+              :middleware [;; query-params & form-params
+                           parameters/parameters-middleware
+                           ;; content-negotiation
+                           muuntaja/format-negotiate-middleware
+                           ;; encoding response body
+                           muuntaja/format-response-middleware
+                           ;; exception handling
+                           exception/exception-middleware
+                           ;; decoding request body
+                           muuntaja/format-request-middleware
+                           ;; coercing response bodys
+                           coercion/coerce-response-middleware
+                           ;; coercing request parameters
+                           coercion/coerce-request-middleware]}})
+    (ring/routes
+      (ring/create-default-handler))))
+
+(defn start []
+  (jetty/run-jetty #'app {:port 3000, :join? false})
+  (println "server running in port 3000"))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/banking_api/core_test.clj
+++ test/banking_api/core_test.clj
@@ -1,8 +1,25 @@
 (ns banking-api.core-test
-  (:require [expectations.clojure.test :as t :refer [defexpect expect expecting]]
-            [banking-api.core :refer :all]))
+  (:require [banking-api.core :as sut]
+            [cheshire.core :as json]
+            [clojure.java.io :as io]
+            [expectations.clojure.test :as t :refer [defexpect expect expecting]]
+            [ring.mock.request :as mock]))
 
-(defexpect first-test
+(defn- parse-json
+  [body]
+  (cond
+    (string? body) (json/parse-string body keyword)
+    (some? body) (json/parse-stream (io/reader body) keyword)))
+
+(defn- test-handler
+  [request]
+  (let [handler sut/app
+        response (handler request)]
+    (update response :body parse-json)))
+
+(defexpect find-account-by-id
   (expecting
-    "check inc fn"
-    (expect 2 (inc 1))))
+    "find an account"
+    (let [{:keys [status body] :as response} (-> (mock/request :get "/account/1")
+                                                 (test-handler))]
+      (expect {:account-number 1 :balance 11 :name "account 1"} body))))
